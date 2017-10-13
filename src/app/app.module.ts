681209db8282ac6b2db9a25e0171dbb4b2a023e6
Joey Burgett <joey.burgett@getbuilt.com>
2017-10-13 17:55:45 -0500

    Ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { ComponentsModule } from '../components/components.module';
+<<<<<<< HEAD
 import { Geolocation } from '@ionic-native/geolocation';
 
 import { StorePage } from '../pages/store/store';
+=======
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+>>>>>>> Derp
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,195 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: any = [
+    {
+      name: 'Heaths Crackhouse',
+      expanded: false,
+      id: 1,
+      address: {
+        address1: '123 First Street',
+        address2: null,
+        city: 'Buffalo',
+        state: 'NY',
+        zipcode: '90210',
+        coordinates: {
+          longitude: -94.2,
+          latitude: 39
+        }
+      }
+    }, {
+      name: 'Jonathon Dropouts',
+      expanded: false,
+      id: 2,
+      address: {
+        address1: '1 Nowhere Road',
+        address2: null,
+        city: 'Boise',
+        state: 'ID',
+        zipcode: '12345',
+        coordinates: {
+          longitude: -94.1,
+          latitude: 39.5
+        }
+      }
+    }, {
+      name: 'Wallys Dingleberries',
+      id: 3,
+      expanded: false,
+      address: {
+        address1: '911 PSN SDK WAY',
+        address2: null,
+        city: 'St. Paul',
+        state: 'MN',
+        zipcode: '23232',
+        coordinates: {
+          longitude: -94.0008,
+          latitude: 39.3
+        }
+      }
+    }, {
+      name: 'Wallys Dingleberries',
+      id: 4,
+      expanded: false,
+      address: {
+        address1: '911 PSN SDK WAY',
+        address2: null,
+        city: 'St. Paul',
+        state: 'MN',
+        zipcode: '23232',
+        coordinates: {
+          longitude: -94.0008,
+          latitude: 39.3
+        }
+      }
+    }, {
+      name: 'Wallys Dingleberries',
+      id: 5,
+      expanded: false,
+      address: {
+        address1: '911 PSN SDK WAY',
+        address2: null,
+        city: 'St. Paul',
+        state: 'MN',
+        zipcode: '23232',
+        coordinates: {
+          longitude: -94.0008,
+          latitude: 39.3
+        }
+      }
+    }];
+  itemExpandHeight: number = 300;
+  markers: any = [];
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(platform: Platform, public navCtrl: NavController, geolocation: Geolocation) {
+    // Reset markers
+    this.markers = [];
+
+    platform.ready()
+      .then(() => {
+        // Try Browser Mode First
+        navigator.geolocation.getCurrentPosition(position => {
+
+          this.loadMap(position.coords);
+
+          this.items.forEach(item => {
+            this.addMarker(item.address.coordinates);
+          });
+
+          this.fitBounds();
+        }, () => {
+          // Switch to Geolocation plugin
+          geolocation.getCurrentPosition().then(position => {
+
+            this.loadMap(position.coords);
+
+            this.items.forEach(item => {
+              this.addMarker(item.address.coordinates);
+            });
+
+            this.fitBounds();
+          })
+        });
+      });
+  }
+
+  fitBounds() {
+
+    let bounds = new google.maps.LatLngBounds();
+    for (let index = 0; index < this.markers.length; index++) {
+      bounds.extend(this.markers[index].getPosition());
+    }
+
+    google.maps.event.addListenerOnce(this.map, 'bounds_changed', function() {
+      this.setZoom(this.getZoom()-1);
+
+      if (this.getZoom() > 15) {
+        this.setZoom(15);
+      }
+
+      this.map.setCenter(bounds.getCenter());
+    });
+
+    this.map.findBounds(bounds);
+  }
+
+  expandItem(item) {
+
+    this.items.map(listItem => {
+
+      if (item == listItem) {
+        listItem.expanded = !listItem.expanded;
+      } else {
+        listItem.expanded = false;
+      }
+
+      return listItem;
+
+    });
+
+//    this.loadMap(item.address.coordinates);
+  }
+
+  loadMap(position: any) {
+
+    let latLng = new google.maps.LatLng(position.latitude, position.longitude);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  addMarker(position: any) {
+
+    let marker = new google.maps.Marker({
+      animation: google.maps.Animation.DROP,
+      position: new google.maps.LatLng(position.latitude, position.longitude),
+      map: this.map
+    });
+
+    this.markers.push(marker);
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content) {
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 
 }
